bc7014c22316fcbd068c7dc48a8b6c849398ce88
rafaelvp-db <rafaelvp-db>
2022-10-17 05:57:28 +0000

    transition

--- notebooks/02_machine_learning.py
+++ notebooks/02_machine_learning.py
@@ -32,84 +32,7 @@ target = "bad_loan"
 
 # COMMAND ----------
 
-df = spark.sql("select * from robeco.loan")
-
-df = df.select(
-    "loan_status",
-    "int_rate",
-    "revol_util",
-    "issue_d",
-    "earliest_cr_line",
-    "emp_length",
-    "verification_status",
-    "total_pymnt",
-    "loan_amnt",
-    "grade",
-    "annual_inc",
-    "dti",
-    "addr_state",
-    "term",
-    "home_ownership",
-    "purpose",
-    "application_type",
-    "delinq_2yrs",
-    "total_acc",
-)
-
-df = df.filter(
-    df.loan_status.isin(["Default", "Charged Off", "Fully Paid"])
-).withColumn("bad_loan", (~(df.loan_status == "Fully Paid")).cast("string"))
-
-df = (
-    df.withColumn("int_rate", regexp_replace("int_rate", "%", "").cast("float"))
-    .withColumn(
-        "revol_util", regexp_replace("revol_util", "%", "").cast("float")
-    )
-    .withColumn("issue_year", substring(df.issue_d, 5, 4).cast("double"))
-    .withColumn(
-        "earliest_year", substring(df.earliest_cr_line, 5, 4).cast("double")
-    )
-)
-df = df.withColumn("credit_length_in_years", (df.issue_year - df.earliest_year))
-
-df = df.withColumn(
-    "emp_length",
-    trim(regexp_replace(df.emp_length, "([ ]*+[a-zA-Z].*)|(n/a)", "")),
-)
-df = df.withColumn(
-    "emp_length", trim(regexp_replace(df.emp_length, "< 1", "0"))
-)
-df = df.withColumn(
-    "emp_length",
-    trim(regexp_replace(df.emp_length, "10\\+", "10")).cast("float"),
-)
-
-df = df.withColumn(
-    "verification_status",
-    trim(regexp_replace(df.verification_status, "Source Verified", "Verified")),
-)
-
-df = df.withColumn("net", round(df.total_pymnt - df.loan_amnt, 2))
-df = df.select(
-    "bad_loan",
-    "term",
-    "home_ownership",
-    "purpose",
-    "addr_state",
-    "verification_status",
-    "application_type",
-    "loan_amnt",
-    "emp_length",
-    "annual_inc",
-    "dti",
-    "delinq_2yrs",
-    "revol_util",
-    "total_acc",
-    "credit_length_in_years",
-    "int_rate",
-    "net",
-    "issue_year",
-)
+df = spark.table()
 
 # COMMAND ----------
 
@@ -149,3 +72,7 @@ with mlflow.start_run(run_name="Training"):
   )
   mlflow.set_tag("action", "training")
   mlflow.sklearn.eval_and_log_metrics(cl, x_test, y_test, prefix="val_")
+
+# COMMAND ----------
+
+
--- notebooks/03_promote_model.py
+++ notebooks/03_promote_model.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 from mlflow.tracking import MlflowClient
 
-model_name = "lending_club_credit_default"
+model_name = "lending_club_random_forest"
 client = MlflowClient()
 model_version = client.get_registered_model(model_name)
 latest_version = model_version.latest_versions[-1]
@@ -20,10 +20,12 @@ latest_version
 if latest_version.description == '':
   raise ValueError("Invalid model description - please add a valid text description for the model")
 
-
 # COMMAND ----------
 
-client.update_model_version(name = model_name, description = "This is a LightGBM model for predicting loan credit risk", version = latest_version.version)
+client.update_model_version(
+  name = model_name, description = "This is a RandomForests model for predicting loan credit risk",
+  version = latest_version.version
+)
 
 # COMMAND ----------
 
@@ -48,7 +50,7 @@ if latest_version.tags == {}:
 client.set_model_version_tag(
   name = model_name,
   key = "dataset",
-  value = "loanstats.train",
+  value = "loanstats_train",
   version = latest_version.version
 )
 
@@ -72,17 +74,26 @@ else:
 # COMMAND ----------
 
 import mlflow
+import pandas as pd
 
-def test_prediction(run_id = "d39001405ae94d18986c4213399d771f"):
+def handle_cat_types(df: pd.DataFrame):
+  for col in df.columns:
+    if df.dtypes[col] == "object":
+      df[col] = df[col].astype("category").cat.codes
+    df[col] = df[col].fillna(0)
+  return df
+
+def test_prediction(run_id):
   model = mlflow.pyfunc.load_model(model_uri = f"runs:/{run_id}/model")
   test_data = table("loanstats_train").toPandas()
+  test_data = handle_cat_types(test_data)
   pred = model.predict(test_data.drop("bad_loan", axis=1).sample(10))
   print(f"Predictions: {pred}")
   assert pred is not None
   
-test_prediction()
+test_prediction(run_id = latest_version.run_id)
 
 # COMMAND ----------
 
 # DBTITLE 1,Transition to Staging
-client.transition_model_version_stage(model_name, latest_version.version, stage="Staging")
+client.r(model_name, latest_version.version, stage="Staging")
